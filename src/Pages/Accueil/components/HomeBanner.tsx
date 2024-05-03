c093df5e9c5ecc205a472e1993d142347c6c090c
Elliot Saint-André <elliot.standre@gmail.com>
2024-05-03 23:16:48 +0200

    [FIX] Homebanner

--- src/Pages/Accueil/components/HomeBanner.tsx
+++ src/Pages/Accueil/components/HomeBanner.tsx
@@ -1,11 +1,11 @@
-import image from "../../../assets/accueilHC.jpg";
+import image from "../../../assets/HCDarkOp.jpg";
 
 const HomeBanner = () => {
   return (
     <div className="font-dilgante relative h-150">
-      <img src={image} alt="accueil" className="w-full h-150 object-cover brightness-75" />
+      <img src={image} alt="accueil" className="w-full h-150 object-cover" />
       <div className="w-11/12 text-white absolute-centering">
-        <h1 className="text-5xl mb-4 font-bold uppercase tablet:text-6xl">Bienvenue sur SHARY COOKING !</h1>
+        <h1 className="text-5xl mb-8 font-bold uppercase tablet:text-[4rem]">Bienvenue sur SHARY COOKING !</h1>
         <span className="text-xl tablet:text-2xl">Venez ajouter votre grain de sel...</span>
       </div>
     </div>

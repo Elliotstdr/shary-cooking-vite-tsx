c8a0ba31852bc6740779b6a65acc5c13d74f1a0f
Elliot Saint-André <elliot.standre@gmail.com>
2024-05-03 23:29:11 +0200

    [FIX] try

--- src/Components/ui/loader.tsx
+++ src/Components/ui/loader.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const Loader = (props: Props) => {
   return (
-    <div className={`w-full z-50 bg-transparent text-center py-8 ${props.className}`}>
+    <div className={`w-full z-50 bg-transparent text-center py-8 ${props.className || ""}`}>
       <span className="
         size-14 rounded-full inline-block border-t-4 border-t-orange border-r-4 border-r-transparent animate-loader
         after:absolute after:left-0 after:top-0 after:size-14 after:rounded-full after:border-l-4 after:border-l-green 

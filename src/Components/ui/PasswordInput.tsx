ae5e4d4033d7687d95f8222e27c4a7c878a8d724
Elliot Saint-André <elliot.standre@gmail.com>
2024-05-10 12:59:34 +0200

    [FIX] show password

--- src/Components/ui/PasswordInput.tsx
+++ src/Components/ui/PasswordInput.tsx
@@ -13,19 +13,16 @@ export const PasswordInput = forwardRef<HTMLInputElement, InputTextProps>(({ ...
         autoComplete="new-password"
         className={`!pr-8 ${props.className}`}
       />
-      <button
-        className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-        onClick={(e) => {
-          e.preventDefault()
-          setShowPassword((prev) => !prev)
-        }}
+      <div
+        className="absolute flex items-center right-0 top-0 h-full px-3 py-2 hover:bg-transparent cursor-pointer"
+        onClick={() => setShowPassword((prev) => !prev)}
       >
         {showPassword ? (
           <div className="pi pi-eye h-4 w-4 text-icon align-middle"></div>
         ) : (
           <div className="pi pi-eye-slash h-4 w-4 text-icon align-middle"></div>
         )}
-      </button>
+      </div>
     </div>
   )
 },
